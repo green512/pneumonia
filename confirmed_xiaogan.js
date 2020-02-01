4109abdb768bcb8065fb4ad339a2882b9317bda7
green512 <green512@163.com>
2020-02-01 11:05:39 +0800

    Add files via upload
    增加疫情热力图，上海、天津、孝感疫情图

---
+++ confirmed_xiaogan.js
@@ -0,0 +1,52 @@
+const LAST_UPDATE = "2020.02.01 09:00:10"; 
+
+const DATA = {
+  "420900": {
+    "confirmedCount": 628,
+    "cityName": "孝感市",
+    "deadCount": 12,
+    "color": "#73181B"
+  },
+  "420902": {
+    "confirmedCount": 227,
+    "cityName": "孝南区",
+    "deadCount": 1,
+    "color": "#73181B"
+  },
+  "420984": {
+    "confirmedCount": 148,
+    "cityName": "汉川市",
+    "deadCount": 7,
+    "color": "#73181B"
+  },
+  "420981": {
+    "confirmedCount": 29,
+    "cityName": "应城市",
+    "deadCount": 0,
+    "color": "#E04B49"
+  },
+  "420923": {
+    "confirmedCount": 33,
+    "cityName": "云梦县",
+    "deadCount": 1,
+    "color": "#E04B49"
+  },
+  "420982": {
+    "confirmedCount": 108,
+    "cityName": "安陆市",
+    "deadCount": 2,
+    "color": "#E04B49"
+  },
+  "420922": {
+    "confirmedCount": 54,
+    "cityName": "大悟县",
+    "deadCount": 0,
+    "color": "#E04B49"
+  },
+  "420921": {
+    "confirmedCount": 29,
+    "cityName": "孝昌县",
+    "deadCount": 1,
+    "color": "#E04B49"
+  }
+}
